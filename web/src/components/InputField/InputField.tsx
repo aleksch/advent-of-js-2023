7c98aaf50d94756eeb95dacd24b9bd551a9b7741
Alex Chernyshev <sanocher@gmail.com>
2023-12-22 17:54:12 +0500

    ability to show and hide password

--- web/src/components/InputField/InputField.tsx
+++ web/src/components/InputField/InputField.tsx
@@ -1,14 +1,36 @@
+import { useState } from 'react'
+
+import Icon from '../Icon/Icon'
+
 type Props = {
   name: string
   label: string
   type?: 'text' | 'email' | 'password'
 }
 const InputField = ({ name, label, type = 'text' }: Props) => {
+  const [isShowPassword, setIsShowPassword] = useState(false)
+  const _type = isShowPassword && type === 'password' ? 'text' : type
+  const handleToggleShowPassword = () => setIsShowPassword((prev) => !prev)
+
   return (
     <div className="relative">
       {/* label should be before input */}
       <label htmlFor={name}>{label}</label>
-      <input type={type} id={name} placeholder="" autoComplete="off" />
+      <input type={_type} id={name} placeholder="" autoComplete="off" />
+      {type === 'password' && (
+        <button
+          className="absolute right-8 top-8"
+          onClick={handleToggleShowPassword}
+        >
+          {type === 'password' && (
+            <Icon
+              name={isShowPassword ? 'eyeOpened' : 'eyeClosed'}
+              color="black"
+              size="lg"
+            />
+          )}
+        </button>
+      )}
     </div>
   )
 }
